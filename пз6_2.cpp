4118cace97d4d91e91cb276f1199bca45a8f8823
Elizaveta-Ag <elizavetaageeva882@gmail.com>
2026-04-03 12:21:35 +0300

    Create пз6_2.cpp

---
+++ пз6_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  int b;
+  cin >> a;
+  cin >> b;
+if (a < b){
+cout << "Number " << b << " is bigger";
+}
+else{
+ cout << "Number " << a << " is bigger";
+}
+return 0;
+}
